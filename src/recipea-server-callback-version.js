d9c2268146826952956129a936dd38af5f860190
Hailey <137557225+officiallyhailey@users.noreply.github.com>
2023-11-29 17:42:01 -0500

    Completed Tasks

--- src/recipea-server-callback-version.js
+++ src/recipea-server-callback-version.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const fs = require("fs");
+const fs = require("fs").promises;
 
 const app = express();
 
@@ -9,30 +9,44 @@ app.listen(3000, () => {
   console.log("Server listening on port 3000");
 });
 
+
+
+const saveNote = async (newNote) => {
+  const data = await fs.readFile("../data/recipea-data.json", "utf8");
+  const notes = [...JSON.parse(data), newNote];
+  const jsonVersion = JSON.stringify(notes, null, 2);
+  await fs.writeFile("../data/recipea-data.json", jsonVersion, "utf8");
+};
+
+const deleteRecipe = async (id) => {
+  const data = await fs.readFile("../data/recipea-data.json", "utf8");
+  const notes = JSON.parse(data).filter((note, i) => i !== id);
+  const jsonVersion = JSON.stringify(notes, null, 2);
+  await fs.writeFile("../data/recipea-data.json", jsonVersion, "utf8");
+};
+
 app.get("/find-recipes", (req, res) => {
   fs.readFile("../data/recipea-data.json", "utf8", (err, data) => {
     res.send(data);
   });
 });
 
-app.get("/find-recipe/:id", (req, res) => {
+app.get("/find-recipe/:id", async (req, res) => {
   const id = Number(req.params.id);
-  fs.readFile("../data/recipea-data.json", "utf8", (err, data) => {
-    const recipes = JSON.parse(data);
-    const recipe = recipes[id];
-    const jsonRecipe = JSON.stringify(recipe, null, 2);
-    res.send(jsonRecipe);
+  const data = await fs.readFile("../data/recipea-data.json", "utf8");
+  const recipes = JSON.parse(data);
+  const recipe = recipes[id];
+  const jsonRecipe = JSON.stringify(recipe, null, 2);
+  res.send(jsonRecipe);
   });
+
+app.get("/create-recipe", async (req, res) => {
+  await saveNote({name: req.body.name, cookingMethod: req.body.cookingMethod, ingredients: req.body.ingredients});
+  res.send("Recipe successfully added to the file!");
+})
+
+app.get("/trash-recipe/:id", async (req, res) => {
+  await deleteRecipe(Number(req.params.id));
+  res.send("Successfully deleted note.");
 });
 
-app.get("/trash-recipe/:id", (req, res) => {
-  const id = Number(req.params.id);
-  fs.readFile("../data/recipea-data.json", "utf8", (err, data) => {
-    const recipes = JSON.parse(data);
-    recipes.splice(id, 1);
-    const jsonRecipes = JSON.stringify(recipes, null, 2);
-    fs.writeFile("../data/recipea-data.json", jsonRecipes, (err) => {
-      res.send("Recipe with " + id + " has been deleted.");
-    });
-  });
-});
